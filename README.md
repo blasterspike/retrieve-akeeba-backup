b5b023167a3b30417dc4b34c0480bc98261354c0
Massimo Vannucci <massimo.vannucci@gmail.com>
2016-03-12 17:45:46 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,114 @@
+# retrieve-akeeba-backup
+
+#### Table of Contents
+
+1. [Description](#description)
+2. [Usage](#usage)
+3. [The ini file](#the-ini-file)
+   3.1. [The structure](#the-structure)
+   3.2. [Example](#example)
+4. [Retention](#retention)
+   4.1. [Short term](#short-term)
+   4.2. [Long term](#long-term)
+5. [Limitations](#limitations)
+
+## Description
+
+This is a script to:
+- trigger an Akeeba Backup in Joomla
+- download locally the file generated using FTP
+- store the file based on a retention
+
+At the end of the process, the script will send an e-mail with the total time.
+
+The script reads all the configurations from an ini file and is intended to be run every day.
+
+## Usage
+
+Provide the full path to the script so it will automatically read the ini file from the same directory.
+
+```bash
+python /usr/local/bin/retrieve_akeeba_backup.py
+```
+
+If you want to redirect the output
+
+```bash
+python /usr/local/bin/retrieve_akeeba_backup.py >> /usr/local/bin/retrieve_akeeba_backup.log 2>&1
+```
+
+## The ini file
+
+The ini file must be called *settings.ini* and must be place in the same directory where the script is.
+All the fields are mandatory.
+
+### The structure
+
+```ini
+[credentials]
+ftp_server = <FTP server>
+username = <FTP username>
+password = <FTP password>
+
+[settings]
+remote_backup_path = <Path on the FTP server to the backup folder of Akeeba Backup>
+trigger_url = <The URL that you need to visit in order to start the backup with Akeeba Backup.
+               You can find this string going in backend of Joomla to Components > Akeeba Backup
+               > Scheduling Information > Url you want to execute>
+short_term_retention =  <Number of days that you want to keep the backup>
+long_term_retention = <Number of weeks that you want to keep the backup.
+                       Only the Sunday backup will be considered to be stored>
+short_term_path = <Where you want to store your daily backups>
+long_term_path = <Where you want to store your weekly backups>
+
+[e-mail]
+sender = <e-mail address of the sender>
+receiver = <e-mail address of the receiver>
+domain = <Domain name of your website. Used in the e-mail notification>
+smtp_server = <SMTP server name>
+```
+
+### Example
+
+```ini
+[credentials]
+ftp_server = ftp.domain.com
+username = jtitor
+password = REYuweqATr35#uc
+
+[settings]
+remote_backup_path = /administrator/components/com_akeeba/backup/
+trigger_url = https://website.com/index.php?option=com_akeeba&view=backup&key=fe6aWr2cubrapr5mamuf
+short_term_retention = 7
+long_term_retention = 26
+short_term_path = /volume1/Backup/Automatic/website/Daily/
+long_term_path = /volume1/Backup/Automatic/website/Long term/
+
+[e-mail]
+sender = example_email@gmail.com
+receiver = example_email@gmail.com
+domain = website.com
+smtp_server = localhost
+```
+
+## Retention
+
+### Short term
+
+On the short term retention, files will be kept only for the specified days.
+Example: if you run the script every day and you have set the short_term_retention to 7, all the files in the short_term_path older
+than 7 days will be deleted.
+
+### Long term
+
+On the long term retention, files will be kept only for the specified weeks.
+Only the backup taken on Sunday will be considered.
+Example: if you run the script every day and you have set the long_term_retention to 26, all the files in the long_term_path older
+than 26 weeks will be deleted.
+
+## Limitations
+
+This script has been tested on:
+
+OS: Synolody DSM 5.2-5644
+Python: 2.7.9
---
+++ retrieve_akeeba_backup.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+# vim: autoindent tabstop=4 shiftwidth=4 expandtab softtabstop=4 filetype=python
+# -*- coding: utf-8 -*-
+# -*- Mode: Python -*-
+# Version: 1.0
+# Author: Massimo Vannucci
+
+import ftplib
+import logging
+import ConfigParser
+import requests
+import os
+import time
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(text, sender, receiver, domain, smtp_server):
+    msg = MIMEText(text)
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg['Subject'] = 'Backup of ' + domain + ' completed'
+
+    s = smtplib.SMTP(smtp_server)
+    s.sendmail(sender, receiver, msg.as_string())
+    s.quit()
+
+
+def rotation(filename, short_term_retention, long_term_retention, short_term_path, long_term_path):
+    for file in os.listdir(short_term_path):
+        # Get the list of files older than 7 days
+        if os.stat(os.path.join(short_term_path, file)).st_mtime < time.time() - int(short_term_retention) * 86400:
+            os.remove(os.path.join(short_term_path, file))
+            logging.info('Removed ' + file)
+
+    # Every Sunday I keep a copy stored for 6 months
+    if time.strftime('%w', time.gmtime()) == 0:
+        for file in os.listdir(long_term_path):
+            # Like the previous command but multiplied by 26 weeks
+            if os.stat(os.path.join(long_term_path, file)).st_mtime < time.time() - int(long_term_retention) * 7 * 86400:
+                os.remove(os.path.join(long_term_path, file))
+                logging.info('Removed ' + file)
+        os.system('cp ' + filename + ' ' + long_term_path)
+        logging.info(filename + ' stored in ' + long_term_path)
+
+    os.system('mv ' + filename + ' ' + short_term_path)
+    logging.info(filename + ' stored in ' + short_term_path)
+
+
+def retrieve_from_ftp(ftp_server, username, password, remote_backup_path, domain):
+    ftps = ftplib.FTP_TLS(ftp_server)
+    ftps.auth()
+    ftps.prot_p()
+    ftps.login(username, password)
+    logging.info('Logged into FTP')
+    ftps.cwd(remote_backup_path)
+
+    filename = ftps.nlst('site-' + domain + '-*')[0]
+    logging.info('Downloading file ' + filename)
+
+    with open(filename, 'wb') as f:
+        ftps.retrbinary('RETR ' + filename, f.write)
+    logging.info(filename + ' downloaded')
+
+    # Remove file in web space
+    ftps.delete(filename)
+
+    return filename
+
+
+def main():
+
+    start_time = time.time()
+
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s: %(message)s')
+
+    # Read settings from ini file
+    settings = ConfigParser.RawConfigParser(allow_no_value=True)
+    ini_file = os.path.dirname(__file__) + '/settings.ini'
+    settings.read(ini_file)
+    # Set the variables
+    ftp_server = settings.get('credentials', 'ftp_server')
+    username = settings.get('credentials', 'username')
+    password = settings.get('credentials', 'password')
+    remote_backup_path = settings.get('settings', 'remote_backup_path')
+    trigger_url = settings.get('settings', 'trigger_url')
+    short_term_retention = settings.get('settings', 'short_term_retention')
+    long_term_retention = settings.get('settings', 'long_term_retention')
+    short_term_path = settings.get('settings', 'short_term_path')
+    long_term_path = settings.get('settings', 'long_term_path')
+    sender = settings.get('e-mail', 'sender')
+    receiver = settings.get('e-mail', 'receiver')
+    domain = settings.get('e-mail', 'domain')
+    smtp_server = settings.get('e-mail', 'smtp_server')
+
+    logging.info('Hitting the URL to call Akeeba Backup')
+    session = requests.session()
+    session.max_redirects = 10000
+    session.get(trigger_url)
+    logging.info('Backup created')
+
+    filename = retrieve_from_ftp(ftp_server,
+                                 username,
+                                 password,
+                                 remote_backup_path,
+                                 domain)
+    rotation(filename,
+             short_term_retention,
+             long_term_retention,
+             short_term_path,
+             long_term_path)
+
+    end_time = time.time()
+    diff_time = end_time - start_time
+    total_time = time.strftime('%H:%M:%S' , time.gmtime(diff_time))
+
+    text = filename + ' backed up in ' + total_time
+    send_mail(text, sender, receiver, domain, smtp_server)
+
+    logging.info('Backup procedure completed')
+
+
+if __name__ == "__main__":
+    main()
