5a2106245a4c6e31502b1fd8692a711f20e20ea1
Massimo Vannucci <massimo.vannucci@gmail.com>
2016-09-20 12:49:57 +0100

    Update retrieve_akeeba_backup.py
    Simplified path for settings.ini

--- retrieve_akeeba_backup.py
+++ retrieve_akeeba_backup.py
@@ -75,7 +75,7 @@ def main():
 
     # Read settings from ini file
     settings = ConfigParser.RawConfigParser(allow_no_value=True)
-    ini_file = os.path.dirname(__file__) + '/settings.ini'
+    ini_file = 'settings.ini'
     settings.read(ini_file)
     # Set the variables
     ftp_server = settings.get('credentials', 'ftp_server')
