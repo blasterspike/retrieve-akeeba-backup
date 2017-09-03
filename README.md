32709542c501418cf754be231939da3f42247ef0
Massimo Vannucci <massimo.vannucci@gmail.com>
2017-09-03 14:32:46 +0100

    Updated script to be able to connect to remote server usin SSH other than FTP

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 
 This is a script to:  
 - trigger an Akeeba Backup in Joomla  
-- download locally the file generated using FTP  
+- download locally the file generated using SSH or FTP  
 - store the file based on a retention  
 
 At the end of the process, the script will send an e-mail with the total time.  
@@ -24,16 +24,23 @@ The script reads all the configurations from an ini file and is intended to be r
 
 ## Usage
 
-Provide the full path to the script so it will automatically read the ini file from the same directory.
+Provide the full path to the script so it will automatically read the ini file from the same directory.  
+If you want to connect to your server using SSH
 
 ```bash
-python /usr/local/bin/retrieve_akeeba_backup.py
+python /usr/local/bin/retrieve_akeeba_backup.py -m ssh
+```
+
+If you want to connect to your server using FTP
+
+```bash
+python /usr/local/bin/retrieve_akeeba_backup.py -m ftp
 ```
 
 If you want to redirect the output
 
 ```bash
-python /usr/local/bin/retrieve_akeeba_backup.py >> /usr/local/bin/retrieve_akeeba_backup.log 2>&1
+python /usr/local/bin/retrieve_akeeba_backup.py -m ssh >> /usr/local/bin/retrieve_akeeba_backup.log 2>&1
 ```
 
 ## The ini file
@@ -43,11 +50,19 @@ All the fields are mandatory.
 
 ### The structure
 
+If you select to use SSH, you can omit the _[ftp]_ section and vice versa.
+
 ```ini
-[credentials]
+[ftp]
 ftp_server = <FTP server>
 username = <FTP username>
 password = <FTP password>
+
+[ssh]
+ssh_server = <SSH server>
+username = <SSH username>
+port = <SSH port>
+pkey_file = <Path to private key>
 
 [settings]
 remote_backup_path = <Path on the FTP server to the backup folder of Akeeba Backup>
@@ -57,37 +72,14 @@ trigger_url = <The URL that you need to visit in order to start the backup with 
 short_term_retention =  <Number of days that you want to keep the backup>
 long_term_retention = <Number of weeks that you want to keep the backup.
                        Only the Sunday backup will be considered to be stored>
-short_term_path = <Where you want to store your daily backups>
-long_term_path = <Where you want to store your weekly backups>
+short_term_path = <Path to your daily backups>
+long_term_path = <Path to your weekly backups>
 
 [e-mail]
 sender = <e-mail address of the sender>
 receiver = <e-mail address of the receiver>
 domain = <Domain name of your website. Used in the e-mail notification>
 smtp_server = <SMTP server name>
-```
-
-### Example
-
-```ini
-[credentials]
-ftp_server = ftp.domain.com
-username = jtitor
-password = REYuweqATr35#uc
-
-[settings]
-remote_backup_path = /administrator/components/com_akeeba/backup/
-trigger_url = https://website.com/index.php?option=com_akeeba&view=backup&key=fe6aWr2cubrapr5mamuf
-short_term_retention = 7
-long_term_retention = 26
-short_term_path = /volume1/Backup/Automatic/website/Daily/
-long_term_path = /volume1/Backup/Automatic/website/Long term/
-
-[e-mail]
-sender = example_email@gmail.com
-receiver = example_email@gmail.com
-domain = website.com
-smtp_server = localhost
 ```
 
 ## Retention
@@ -102,9 +94,3 @@ Example: if you run the script every day and you have set the short_term_retenti
 On the long term retention, files will be kept only for the specified weeks.  
 Only the backup taken on Sunday will be considered.  
 Example: if you run the script every day and you have set the long_term_retention to 26, all the files in the long_term_path older than 26 weeks will be deleted.
-
-## Limitations
-
-This script has been tested on:  
-OS: Synolody DSM 5.2-5644  
-Python: 2.7.9
--- retrieve_akeeba_backup.py
+++ retrieve_akeeba_backup.py
@@ -5,6 +5,10 @@
 # Version: 1.0
 # Author: Massimo Vannucci
 
+import argparse
+import platform
+import paramiko
+import sys
 import ftplib
 import logging
 import ConfigParser
@@ -13,6 +17,11 @@ import os
 import time
 import smtplib
 from email.mime.text import MIMEText
+import re
+
+if platform.system() == 'Darwin':
+    import keyring
+
 
 def send_mail(text, sender, receiver, domain, smtp_server):
     msg = MIMEText(text)
@@ -30,7 +39,7 @@ def rotation(filename, short_term_retention, long_term_retention, short_term_pat
         # Get the list of files older than 7 days
         if os.stat(os.path.join(short_term_path, file)).st_mtime < time.time() - int(short_term_retention) * 86400:
             os.remove(os.path.join(short_term_path, file))
-            logging.info('Removed ' + file)
+            logging.info('Removed {0}'.format(file))
 
     # Every Sunday I keep a copy stored for 6 months
     if time.strftime('%w', time.gmtime()) == 0:
@@ -38,12 +47,12 @@ def rotation(filename, short_term_retention, long_term_retention, short_term_pat
             # Like the previous command but multiplied by 26 weeks
             if os.stat(os.path.join(long_term_path, file)).st_mtime < time.time() - int(long_term_retention) * 7 * 86400:
                 os.remove(os.path.join(long_term_path, file))
-                logging.info('Removed ' + file)
+                logging.info('Removed {0}'.format(file))
         os.system('cp ' + filename + ' ' + long_term_path)
-        logging.info(filename + ' stored in ' + long_term_path)
+        logging.info('{0} stored in {1}'.format(filename, long_term_path))
 
     os.system('mv ' + filename + ' ' + short_term_path)
-    logging.info(filename + ' stored in ' + short_term_path)
+    logging.info('{0} stored in {1}'.format(filename, short_term_path))
 
 
 def retrieve_from_ftp(ftp_server, username, password, remote_backup_path, domain):
@@ -55,14 +64,52 @@ def retrieve_from_ftp(ftp_server, username, password, remote_backup_path, domain
     ftps.cwd(remote_backup_path)
 
     filename = ftps.nlst('site-' + domain + '-*')[0]
-    logging.info('Downloading file ' + filename)
+    logging.info('Downloading file {0}'.format(filename))
 
     with open(filename, 'wb') as f:
         ftps.retrbinary('RETR ' + filename, f.write)
-    logging.info(filename + ' downloaded')
+    logging.info('{0} downloaded'.format(filename))
 
     # Remove file in web space
     ftps.delete(filename)
+    logging.info('Deleted {0} on the server'.format(filename))
+
+    return filename
+
+
+def retrieve_from_ssh(ssh_server, username, port, pkey_file, remote_backup_path, domain):
+    if platform.system() == 'Darwin':
+        password = keyring.get_password('SSH', pkey_file)
+        key = paramiko.RSAKey.from_private_key_file(pkey_file, password=password)
+    elif platform.system() == 'Linux':
+        key = paramiko.RSAKey.from_private_key_file(pkey_file)
+    ssh = paramiko.SSHClient()
+    ssh.load_system_host_keys()
+    # To avoid the error
+    # paramiko.ssh_exception.SSHException: Server '$server' not found in known_hosts
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=ssh_server, username=username, port=port, pkey=key)
+    logging.info('Connected using SSH')
+    sftp = ssh.open_sftp()
+    # http://docs.paramiko.org/en/1.17/api/sftp.html#paramiko.sftp_client.SFTPClient
+
+    files = sftp.listdir(remote_backup_path)
+    logging.debug('List of files: {0}'.format(files))
+    for file in files:
+        match = re.match(r'site-' + domain + '-.*', file)
+        # This is to avoid match = None
+        if match:
+            filename = match.group(0)
+    logging.info('Downloading file {0}'.format(filename))
+    sftp.get(remote_backup_path + filename, filename)
+    logging.info('{0} downloaded'.format(filename))
+
+    # Remove file in web space
+    sftp.remove(remote_backup_path + filename)
+    logging.info('Deleted {0} on the server'.format(filename))
+
+    sftp.close()
+    ssh.close()
 
     return filename
 
@@ -71,16 +118,30 @@ def main():
 
     start_time = time.time()
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', '-m', help='Select the mode to connect to the server: FTP or SSH', required=True)
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s: %(message)s')
 
     # Read settings from ini file
     settings = ConfigParser.RawConfigParser(allow_no_value=True)
-    ini_file = os.path.dirname(os.path.realpath(__file__)) + '/settings.ini'
+    ini_file = os.path.dirname(__file__) + '/settings.ini'
+    logging.debug('ini file path: {0}'.format(ini_file))
     settings.read(ini_file)
     # Set the variables
-    ftp_server = settings.get('credentials', 'ftp_server')
-    username = settings.get('credentials', 'username')
-    password = settings.get('credentials', 'password')
+    if args.mode == 'ftp':
+        ftp_server = settings.get('ftp', 'ftp_server')
+        username = settings.get('ftp', 'username')
+        password = settings.get('ftp', 'password')
+    elif args.mode == 'ssh':
+        ssh_server = settings.get('ssh', 'ssh_server')
+        username = settings.get('ssh', 'username')
+        port = int(settings.get('ssh', 'port'))
+        pkey_file = settings.get('ssh', 'pkey_file')
+    else:
+        logging.error('No valid mode provided. Specify either ftp or ssh.')
+        sys.exit(1)
     remote_backup_path = settings.get('settings', 'remote_backup_path')
     trigger_url = settings.get('settings', 'trigger_url')
     short_term_retention = settings.get('settings', 'short_term_retention')
@@ -98,11 +159,23 @@ def main():
     session.get(trigger_url)
     logging.info('Backup created')
 
-    filename = retrieve_from_ftp(ftp_server,
-                                 username,
-                                 password,
-                                 remote_backup_path,
-                                 domain)
+    if args.mode == 'ftp':
+        filename = retrieve_from_ftp(ftp_server,
+                                     username,
+                                     password,
+                                     remote_backup_path,
+                                     domain)
+    elif args.mode == 'ssh':
+        filename = retrieve_from_ssh(ssh_server,
+                                     username,
+                                     port,
+                                     pkey_file,
+                                     remote_backup_path,
+                                     domain)
+    else:
+        logging.error('No valid mode provided. Specify either ftp or ssh.')
+        sys.exit(1)
+
     rotation(filename,
              short_term_retention,
              long_term_retention,
@@ -111,7 +184,7 @@ def main():
 
     end_time = time.time()
     diff_time = end_time - start_time
-    total_time = time.strftime('%H:%M:%S' , time.gmtime(diff_time))
+    total_time = time.strftime('%H:%M:%S', time.gmtime(diff_time))
 
     text = filename + ' backed up in ' + total_time
     send_mail(text, sender, receiver, domain, smtp_server)
